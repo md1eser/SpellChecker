9e0804064d8e1566727703a86e75decd75474360
PedroJ03 <pedrojossi03@gmail.com>
2025-10-14 11:27:44 -0300

    tests de  Dictionary y DictionaryTrie

--- src/test/java/edu/isistan/spellchecker/MyTests.java
+++ src/test/java/edu/isistan/spellchecker/MyTests.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
 import java.io.StringReader;
+
+import edu.isistan.spellchecker.corrector.DictionaryTrie;
+import edu.isistan.spellchecker.corrector.Dictionary;
 import edu.isistan.spellchecker.tokenizer.TokenScanner;
 import java.io.IOException;
 
@@ -11,6 +14,10 @@ import java.io.IOException;
 
 /** Cree sus propios tests. */
 public class MyTests {
+    private Dictionary dictionary;
+    private DictionaryTrie dictionaryTrie;
+
+    //TESTS de TOKEN SCANNER---------------------------------------------------------------
     @Test
     public void pruebaEntradaVacia() throws IOException {
         String entrada = "";
@@ -62,6 +69,80 @@ public class MyTests {
             assertEquals(tokenEsperado, scanner.next());
         }
         assertFalse(scanner.hasNext());
+    }
+
+    //TEST de Dictionary (basado en HashSet) -------------------------------------------
+    @Before
+    public void setUpDictionary() throws IOException {
+        String texto = "Palabra uno\ndos DOS tres";
+        TokenScanner scanner = new TokenScanner(new StringReader(texto));
+        dictionary = new Dictionary(scanner);
+    }
+
+    @Test
+    public void pruebaPalabraDentroDiccionario() {
+        assertTrue(dictionary.isWord("uno"));
+    }
+
+    @Test
+    public void pruebaPalabraFueraDiccionario() {
+        assertFalse(dictionary.isWord("cuatro"));
+    }
+
+    @Test
+    public void pruebaTamanioDiccionario() {
+        assertEquals(4, dictionary.getNumWords());
+    }
+
+    @Test
+    public void pruebaPalabraVacia() {
+        assertFalse(dictionary.isWord(""));
+        assertFalse(dictionary.isWord(null));
+    }
+
+    @Test
+    public void pruebaCaseInsensitive() {
+        assertTrue(dictionary.isWord("PALABRA"));
+        assertTrue(dictionary.isWord("PalAbRa"));
+        assertTrue(dictionary.isWord("dos"));
+        assertTrue(dictionary.isWord("DOS"));
+    }
+
+    //TEST de DictionaryTrie (basado en Trie) --------------------------------------------
+    @Before
+    public void setUpDictionaryTrie() throws IOException {
+        String texto = "Palabra uno\ndos DOS tres";
+        TokenScanner scanner = new TokenScanner(new StringReader(texto));
+        dictionaryTrie = new DictionaryTrie(scanner);
+    }
+
+    @Test
+    public void pruebaPalabraDentroDiccionarioTrie() {
+        assertTrue(dictionaryTrie.isWord("uno"));
+    }
+
+    @Test
+    public void pruebaPalabraFueraDiccionarioTrie() {
+        assertFalse(dictionaryTrie.isWord("cuatro"));
+    }
+
+    @Test
+    public void pruebaTamanioDiccionarioTrie() {
+        assertEquals(4, dictionaryTrie.getNumWords());
+    }
+
+    @Test
+    public void pruebaPalabraVaciaTrie() {
+        assertFalse(dictionaryTrie.isWord(""));
+        assertFalse(dictionaryTrie.isWord(null));
+    }
+
+    @Test
+    public void pruebaCaseInsensitiveTrie() {
+        assertTrue(dictionaryTrie.isWord("PALABRA"));
+        assertTrue(dictionaryTrie.isWord("PaLaBra"));
+        assertTrue(dictionaryTrie.isWord("dos"));
+        assertTrue(dictionaryTrie.isWord("DOS"));
     }
 }
 
